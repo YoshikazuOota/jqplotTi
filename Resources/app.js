afe0a6f2c0fa3bcc248e994329ee6eb60ca8d308
yoshikazu oota <atticatticattic@gmail.com>
2014-12-14 21:32:10 +0900

    init

---
+++ Resources/app.js
@@ -0,0 +1,35 @@
+var event_name = 'myJqplotEvent';
+
+var data = [[3, 7, 9, 1, 4, 6, 8, 2, 5]];
+var option = {
+  title: 'Plot With Options',
+  axes: {
+    xaxis: {label: "X Axis", pad: 0},
+    yaxis: {label: "Y Axis"}
+  }
+};
+
+var window = Ti.UI.createWindow({
+  backgroundColor: 'white',
+  touchEnabled: false // これを付けないとWebView内でスクロールが発生したりして不都合がある
+});
+
+var webView = Ti.UI.createWebView({
+  url: 'html/jqplot.html',
+  top: '30dp',
+  height: '300dp',
+  width: '300dp'
+});
+
+
+var chartFunc = function () {
+  // 注: webview側のJSには文字列として値を渡すので、オブジェクトは指定できません
+  webView.evalJS("jqplot_ti(" + JSON.stringify(data) + ", " + JSON.stringify(option) + ")");
+  Titanium.App.removeEventListener(event_name, chartFunc);
+};
+Titanium.App.addEventListener(event_name, chartFunc);
+
+
+window.add(webView);
+window.open();
+
